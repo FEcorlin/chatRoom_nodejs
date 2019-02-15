191fe297bc9cd2190a3b153db4a2cbafc073f0d5
johnny <johnny@johnnydeiMac.local>
2019-02-15 16:38:10 +0800

    socket.io 实现双向通信

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.broadcast.emit('hi');
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
+
+app.use(express.static('libs'));
+
+http.listen(5000, function(){
+    console.log('listening on *:5000');
+});
